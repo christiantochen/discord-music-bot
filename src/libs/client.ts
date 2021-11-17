f25cd7f7aeba1cdff90f6fc5647e34d146c0547f
Christianto Chen <christiantochen@users.noreply.github.com>
2021-11-17 20:13:15 +0700

    feat: database

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -3,9 +3,13 @@ import SlashCommandHandler from "../handlers/slashCommandHandler";
 import EventHandler from "../handlers/eventHandler";
 import MusicPlayerHandler from "../handlers/musicPlayerHandler";
 import { Logger } from "tslog";
+import Database from "./database";
+import SettingHandler from "../handlers/settingHandler";
 
 export default class NClient extends Client {
   public log: Logger = new Logger();
+  public database = new Database(this);
+  public settings = new SettingHandler(this);
 
   public slashCommands = new SlashCommandHandler(this);
   public events = new EventHandler(this);
