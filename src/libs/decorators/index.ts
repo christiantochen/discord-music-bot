fd3b7c8e0bc40f53a5998571b1148e710cbb8ad9
Christianto Chen <christiantochen@users.noreply.github.com>
2021-11-22 03:37:06 +0700

    chore: update

--- src/libs/decorators/index.ts
+++ src/libs/decorators/index.ts
@@ -1,14 +1,21 @@
 import { CommandInteraction } from "discord.js";
+import createEmbed from "../utils/createEmbed";
 
 export default function decorator<
-	T extends (interaction: CommandInteraction) => Promise<boolean | undefined>
+	T extends (interaction: CommandInteraction) => Promise<string | undefined>
 >(func: T) {
 	return (_: unknown, __: string, descriptor: PropertyDescriptor) => {
 		const method = descriptor.value;
 		descriptor.value = async function (interaction: CommandInteraction) {
-			const passed = await func(interaction);
-			if (passed) await method.call(this, interaction);
-			return;
+			const message = await func(interaction);
+			
+			if (message) {
+				return interaction.editReply({
+					embeds: [createEmbed({ description: message })]
+				});
+			}
+
+			await method.call(this, interaction);
 		};
 	};
 }
